3a98427fd5a72134e8daaf6d3db0ae8b314c505a
togenn <perala.toni@gmail.com>
2022-01-12 12:44:52 +0200

    fixed string formatting function

--- project-src/string_formatting.c
+++ project-src/string_formatting.c
@@ -1,30 +1,36 @@
-#include "string_formatting.h"
 
+#include "string_formatting.h"
+#include <stdio.h>
+#include <string.h>
 
 static void int2string(uint16_t num, char *buffer) {
 
 	sprintf(buffer, "%d", num);
-
-}
-
-void format_date_time(char *buffer, date_time_type *date_time) {
-	char format[] = "%02d:%02d %02d/%02d/20%d";
-	sprintf(buffer, format, (int) date_time->hours, (int) date_time->minutes,
-			(int) date_time->date,
-			(int) date_time->month, (int) date_time->year);
 }
 
 void format_dht22_values(char *buffer, uint16_t value) {
 	uint8_t decimal = value % 10;
 	uint8_t integer = value / 10;
 
-	char integer_str[2];
+	char integer_str[3];
 	int2string(integer, integer_str);
+    integer_str[2] = '\0';
 
-	char decimal_str[1];
+	char decimal_str[2];
 	int2string(decimal, decimal_str);
+    decimal_str[1] = '\0';
 
 	char comma[] = ",";
-	strncpy(buffer, strcat(strcat(integer_str, comma), decimal_str), 4);
-	buffer[4] = '\0';
+	strcat(buffer, integer_str);
+    strcat(buffer, comma);
+    strcat(buffer, decimal_str);
+	
+}
+
+int main() {
+    char temp[5] = "\0";
+    format_dht22_values(temp, 224);
+
+    printf("%s\n", temp);
+
 }
--- project-src/string_formatting.h
+++ project-src/string_formatting.h
@@ -1,6 +1,9 @@
 #ifndef __STRING_FORMATTING_H
 #define __STRING_FORMATTING_H
 
+#include "stdint.h"
+#include "RTC.h"
+
 format_dht22_values(char *buffer, uint16_t value);
 format_date_time(char *buffer, date_time_type *date_time);
 
